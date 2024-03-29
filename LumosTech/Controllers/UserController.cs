1c569feeafbbf33b8ff1baa8cdd49f837e406e48
Felipe Aparecido Martins <fmi433812@gmail.com>
2024-03-29 03:25:42 -0300

    Datatable created

--- LumosTech/Controllers/UserController.cs
+++ LumosTech/Controllers/UserController.cs
@@ -4,6 +4,7 @@ using Lumos.Application.Interfaces.Management;
 using Lumos.Data.Models.Management;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System.ComponentModel.DataAnnotations;
 
 namespace Lumos.Mvc.Controllers
@@ -32,11 +33,64 @@ namespace Lumos.Mvc.Controllers
             return View(new UserDto());
         }
 
-        public IActionResult GetAllUsers(int page, int pageSize)
+        //public IActionResult GetAllUsers(int page, int pageSize)
+        //{
+        //    //var users = _userAppService.GetAllPaginatedAsync(page, pageSize);
+
+        //    var users = new List<User>
+        //    {
+        //        new User { FullName = "João Silva", Cpf = "123.456.789-00", Email = "joao@example.com" },
+        //        new User { FullName = "Maria Souza", Cpf = "987.654.321-00", Email = "maria@example.com" }
+        //    };
+
+        //    return Json(new { data = users });
+        //}
+
+        //    public IActionResult GetAllUsers(int draw, int start, int length, int page, int pageSize)
+        //    {
+        //        // Simula uma busca paginada (substitua isso pela lógica real de busca paginada)
+        //        var totalRecords = 100; // Total de registros no banco de dados
+        //        var users = new List<User>
+        //{
+        //    new User { FullName = "João Silva", Cpf = "123.456.789-00", Email = "joao@example.com" },
+        //    new User { FullName = "Maria Souza", Cpf = "987.654.321-00", Email = "maria@example.com" }
+        //};
+
+        //        var filteredUsers = users.Skip(start).Take(length).ToList();
+
+        //        return Json(new
+        //        {
+        //            draw = draw,
+        //            recordsTotal = totalRecords,
+        //            recordsFiltered = totalRecords, // Se não houver filtro, é o mesmo que recordsTotal
+        //            data = filteredUsers
+        //        });
+        //    }
+
+        [HttpPost]
+        public IActionResult GetUserData()
         {
-            var users = _userAppService.GetAllPaginatedAsync(page, pageSize);
-            return Json(new { data = users });
+            // Dados fictícios para exemplo
+            var users = new List<object>
+            {
+                new { Id = 1, Nome = "João", CPF = "123.456.789-00", Email = "joao@example.com" },
+                new { Id = 2, Nome = "Maria", CPF = "987.654.321-00", Email = "maria@example.com" }
+            };
+
+            // Definindo a estrutura de dados esperada pela DataTable
+            var data = new
+            {
+                draw = Request.Form["draw"],
+                recordsTotal = users.Count,
+                recordsFiltered = users.Count,
+                data = users
+            };
+
+            //return Json(data);
+            return Content(JsonConvert.SerializeObject(data), "application/json");
+
         }
+
 
         [HttpPost]
         [AllowAnonymous]
--- LumosTech/wwwroot/js/User/Index.js
+++ LumosTech/wwwroot/js/User/Index.js
@@ -1,3 +1,39 @@
-﻿$(document).ready(function () {
-    $('#userTable').DataTable();
-});
+﻿////$(document).ready(function () {
+////    $('#userTable').DataTable();
+////});
+
+//var _$table = $('#userTable');
+
+//var _$userTable = _$table.DataTable({
+//    "language": {
+//        "url": "//cdn.datatables.net/plug-ins/1.10.24/i18n/Portuguese-Brasil.json"
+//    },
+//    "processing": true,
+//    "serverSide": true,
+//    "ajax": {
+//        "url": "/User/GetAllUsers", 
+//        "type": "POST", 
+//        "dataType": "json",
+//        "data": function (data) {
+//            data.page = data.start / data.length + 1;
+//            data.pageSize = data.length; 
+
+//            console.log(data);
+//            return data;
+//        }
+//    },
+//    "columns": [
+//        { "data": "1" }, 
+//        //{ "data": "Id" }, 
+//        { "data": "fullName" }, 
+//        { "data": "cpf" }, 
+//        { "data": "email" },
+//        {
+//            "data": null,
+//            "render": function (data, type, row, meta) {
+//                return '<button onclick="editUser(' + data.Id + ')">Editar</button>' +
+//                    '<button onclick="deleteUser(' + data.Id + ')">Excluir</button>';
+//            }
+//        }
+//    ]
+//});
